4763794550a8d43988d23814aa0a5b34a6069f5e
Joe DeKeyrel <joedekeyrel@gmail.com>
2023-12-27 17:41:21 -0500

    Reading file instead of importing?

--- src/services/Config.js
+++ src/services/Config.js
@@ -1,3 +1,7 @@
-import Config from '../../config/secrets.json';
+import fs from 'fs';
+
+// import Config from '../../config/secrets.json';
+
+const Config = JSON.parse(fs.readFileSync('../../config/secrets.json', 'utf-8'))
 
 export default Config
