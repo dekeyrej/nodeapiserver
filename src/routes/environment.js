57641370b29ca5ceec038d3e8702863adc93dc79
Joe DeKeyrel <joedekeyrel@gmail.com>
2024-01-07 16:04:48 -0500

    Lower level environmental API; dev and prod config

--- src/routes/environment.js
+++ src/routes/environment.js
@@ -10,7 +10,8 @@ router.get('/', async (req, res) => {
     const output = {
         updateTime: Data.updateTime, 
         updateDate: Data.updateDate, 
-        AQI: Data.AQI, Moon: Data.Moon, 
+        AQI: Data.AQI, 
+        Moon: Data.Moon, 
         Weather: {
             current: Data.Weather.current, 
             hourly: Data.Weather.hourly.slice(0,count), 
@@ -18,4 +19,62 @@ router.get('/', async (req, res) => {
         },
     }
     res.send(output)
-})
+});
+
+router.get('/aqi', async (req, res) => {
+    const output = {
+        updateTime: Data.updateTime, 
+        updateDate: Data.updateDate, 
+        AQI: Data.AQI
+    }
+    res.send(output)
+});
+
+router.get('/moon', async (req, res) => {
+    const output = {
+        updateTime: Data.updateTime, 
+        updateDate: Data.updateDate, 
+        Moon: Data.Moon
+    }
+    res.send(output)
+});
+
+router.get('/weather', async (req, res) => {
+    const output = {
+        updateTime: Data.updateTime, 
+        updateDate: Data.updateDate, 
+        Weather: {
+            current: Data.Weather.current, 
+            hourly: Data.Weather.hourly.slice(0,count), 
+            forecast: Data.Weather.forecast.slice(0,count),
+        }
+    }
+    res.send(output)
+});
+
+router.get('/weather/current', async (req, res) => {
+    const output = {
+        updateTime: Data.updateTime, 
+        updateDate: Data.updateDate, 
+        current: Data.Weather.current
+    }
+    res.send(output)
+});
+
+router.get('/weather/hourly', async (req, res) => {
+    const output = {
+        updateTime: Data.updateTime, 
+        updateDate: Data.updateDate, 
+        hourly: Data.Weather.hourly.slice(0,count)
+    }
+    res.send(output)
+});
+
+router.get('/weather/forecast', async (req, res) => {
+    const output = {
+        updateTime: Data.updateTime, 
+        updateDate: Data.updateDate, 
+        forecast: Data.Weather.forecast.slice(0,count)
+    }
+    res.send(output)
+});
--- src/services/Config.js
+++ src/services/Config.js
@@ -1,6 +1,13 @@
 import fs from 'fs';
 
+let Config
+
 // rassafrassing fs.readFileSync uses paths relative to project root?!?!?!
-const Config = JSON.parse(fs.readFileSync('./config/secrets.json', 'utf-8'));
+if (process.env.DEV == 1) {
+    Config = JSON.parse(fs.readFileSync('./config/secrets.dev.json', 'utf-8'));
+} else {
+    Config = JSON.parse(fs.readFileSync('./config/secrets.json', 'utf-8'));
+}
+
 
 export default Config
